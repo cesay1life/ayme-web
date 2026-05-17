a77d6ac67b4899977d97d8992098688ecfe265cd
César Sainz Ayuso <sainzayusocesar@gmail.com>
2026-05-17 13:56:44 +0200

    Refactor form submission logic and validation

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     await fetch(
 
-        "TU_URL",
+        "https://script.google.com/macros/s/AKfycbySQ28dhW6e3fmUxKSRqvlJy6amBvVkIf41p3pp6ay6u1NIVuPSsh7gizxUWT-4swHjSQ/exec" ,
 
         {
 
